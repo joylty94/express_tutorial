b5bc54ee8f39792967da58c964e642265cd7588e
taeyong_ng <taeyong.lee@ngenius.co.kr>
2019-11-04 14:07:43 +0900

    루트 세션을 활용한 redirect 추가

--- router/main.js
+++ router/main.js
@@ -2,10 +2,20 @@ module.exports = function (app, fs) {
     app.get('/', function (req, res) {
         var sess;
         sess = req.session;
-        res.render('login.html')
+        if(sess.userid){
+            res.redirect('/home')
+        }else{
+            res.render('login.html')
+        }
     });
     app.get('/login', function (req, res) {
-        res.render('login.html');
+        var sess;
+        sess = req.session;
+        if (sess.userid) {
+            sess.redirect('/home')
+        } else {
+            res.render('login.html')
+        }
     });
     app.get('/home', (req, res) => {
         res.render('home.html')
