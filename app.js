017e7ef27b0143281b4def0987611e0c6c3a8f48
taeyong_ng <taeyong.lee@ngenius.co.kr>
2019-11-13 10:29:50 +0900

    api 모듈화

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ sequelize.sync();
 
 // var router = require('./router/main')(app, fs, connection);
 var router = require('./router/main')(app, fs);
+var api = require('./api/index')();
 
 app.set('port', process.env.PORT || 3000);
 
@@ -28,81 +29,13 @@ app.engine('html', require('ejs').renderFile);
 
 app.use(express.static('public'));
 db();
+
 app.use(session({
     store: new RedisStore({ client: redisClient }),
     secret: '@#@$MYSIGN#@$#$',
     resave: false,
     saveUninitialized: true,
 }));
-
-app.post('/api/login', (req, res) => {
-    var sess;
-    sess = req.session;
-
-    fs.readFile(__dirname + "/data/users.json", 'utf8', function(err, data){
-        const { userid, userpass } = req.body;
-        var filelist = fs.readdirSync('./data');
-        var users = JSON.parse(data);
-        var result = {}
-        // console.log(filelist)
-        // console.log(users)
-        if(!users[userid]){
-            result['success'] = 0;
-            result['error'] = "not found";
-            res.json(result);
-            return;
-        }
-
-        if(users[userid]["password"] == userpass){
-            result['success'] = 1;
-            sess.userid = userid;
-            sess.name = users[userid]["name"];
-            res.json(result);
-            return;
-        }else{
-            result["success"] = 0;
-            result["error"] = "incorrect";
-            res.json(result);
-        }
-    })
-});
-
-app.get('/api/logout', function (req, res) {
-    var sess;
-    sess = req.session;
-    var result = {}
-    if (sess.userid) {
-        req.session.destroy(function (err) {
-            if (err) {
-                console.log(err);
-                result["success"] = 0;
-                result["error"] = err;
-                res.json(result)
-            } else {
-                result["success"] = 1;
-                res.json(result)
-            }
-        })
-    }
-})
-
-app.get('/api/list', (req, res) => {
-    fs.readFile(__dirname + "/data/users.json", 'utf8', function (err, data) {
-        var users = JSON.parse(data)
-        res.json(users);
-    });
-})
-
-app.get('/api/getUser/:username', function (req, res) {
-    fs.readFile(__dirname + "/data/users.json", 'utf8', function (err, data) {
-        var users = JSON.parse(data);
-        res.json(users['users'][req.params.username]);
-    });
-});
-
-app.post('/api/signup', function (req, res){
-    const { userid, userpass, username } = req.body;
-})
 
 app.use((req, res, next) => { // 404 처리 부분
     res.status(404).send('일치하는 주소가 없습니다!');
